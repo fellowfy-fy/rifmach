2f864152ce2ab79a67c5a50c147e5bc3e1f0cb95
IgorAndreic <bbcomerc@gmail.com>
2024-10-17 16:07:37 +0300

    черновой благодарность + моб

--- components/ui/stage.tsx
+++ components/ui/stage.tsx
@@ -10,25 +10,52 @@ const images = [
   "/stage4.svg",
 ];
 
+const imagesmob = [
+  "/stage1mob.svg",
+  "/stage2mob.svg",
+  "/stage3mob.svg",
+];
+
 export const Stage = () => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [isMobile, setIsMobile] = useState(false);
+
+  // Функция для проверки размеров экрана
+  const checkIsMobile = () => {
+    if (window.innerWidth <= 768) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
+    }
+  };
+
+  // Устанавливаем слушатель изменения размера окна
+  useEffect(() => {
+    checkIsMobile(); // Проверка при первой загрузке
+    window.addEventListener("resize", checkIsMobile); // Добавление слушателя
+
+    return () => window.removeEventListener("resize", checkIsMobile); // Очистка слушателя при размонтировании компонента
+  }, []);
 
   // Меняем изображение каждые 5 секунд и отключаем цикличность
   useEffect(() => {
-    if (currentImageIndex < images.length - 1) {
+    const selectedImages = isMobile ? imagesmob : images;
+    if (currentImageIndex < selectedImages.length - 1) {
       const interval = setInterval(() => {
         setCurrentImageIndex((prevIndex) => prevIndex + 1);
       }, 5000); // Интервал смены изображения 5 секунд
 
       return () => clearInterval(interval); // Очистка интервала
     }
-  }, [currentImageIndex]);
+  }, [currentImageIndex, isMobile]);
+
+  const selectedImages = isMobile ? imagesmob : images;
 
   return (
     <div className="relative w-full h-screen flex justify-center items-center">
       <motion.img
         key={currentImageIndex}
-        src={images[currentImageIndex]}
+        src={selectedImages[currentImageIndex]}
         alt="Animated Scene"
         className="w-full h-auto"
         initial={{ opacity: 0, scale: 0.8 }} // Начальное состояние (прозрачное и уменьшенное)
