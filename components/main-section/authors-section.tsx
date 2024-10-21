bcad93bbec970fb1e9570a7addc3a400705a9677
fellowfy-fy <zhoraawesome@gmail.com>
2024-10-21 17:24:53 +0300

    Арки и фикс хедера

--- components/main-section/authors-section.tsx
+++ components/main-section/authors-section.tsx
@@ -1,35 +1,42 @@
 import AuthorCard from "../ui/author-card";
-import Carousel from "../ui/carousel"
+import Carousel from "../ui/carousel";
 import Image from "next/image";
 
-  const authors = [
-    <AuthorCard key={1} />,
-    <AuthorCard key={2} />,
-    <AuthorCard key={3} />,
-    <AuthorCard key={4} />,
-    <AuthorCard key={5} />,
-    <AuthorCard key={6} />,
-  ];
+const authors = [
+  <AuthorCard key={1} />,
+  <AuthorCard key={2} />,
+  <AuthorCard key={3} />,
+  <AuthorCard key={4} />,
+  <AuthorCard key={5} />,
+  <AuthorCard key={6} />,
+];
 
 export default function AuthorsSection() {
-    return(
-        <section className="bg-white mx-[17px] md:mx-[134px] pb-[45px] md:py-[25px]">
-            <div className="absolute top-[-5] right-10 hidden md:block">
-                <Image
-                src="/alphabet.svg"
-                alt="Alphabet Background"
-                width={567}
-                height={580}
-                />
-            </div>
-            <h2 className="text-headers text-[24px] md:text-h2 uppercase">
-                Кто будет писать<br />для вас стихи?
-            </h2>
-            <hr className="w-[40px] md:w-[50px] h-[5px] bg-main rounded-sm mt-[10px] md:mt-[22px]" />
-            <p className="text-textsmain text-[12px] md:text-h3 mt-[10px] md:mt-[22px] max-w-[659px]">Наши авторы — лауреаты региональных творческих конкурсов, обладатели премий федерального масштаба, за плечами у них публикации в солидных журналах и альманахах, изданные книги, членство в профессиональных союзах, мы постоянно в тренде, и продолжаем оттачивать свое мастерство.</p>
-            <div className="pt-[83px] md:pt-[102px]">
-                <Carousel items={authors}/>
-            </div>
-        </section>
-    )
+  return (
+    <section className="bg-white mx-[17px] md:mx-[134px] pb-[45px] md:py-[25px]">
+      <div className="absolute top-[-5] right-10 hidden md:block">
+        <Image
+          src="/alphabet.svg"
+          alt="Alphabet Background"
+          width={567}
+          height={580}
+        />
+      </div>
+      <h2 className="text-headers text-[24px] md:text-h2 uppercase">
+        Кто будет писать
+        <br />
+        для вас стихи?
+      </h2>
+      <hr className="w-[40px] md:w-[50px] h-[5px] bg-main rounded-sm mt-[10px] md:mt-[22px]" />
+      <p className="text-textsmain text-[12px] md:text-h3 mt-[10px] md:mt-[22px] max-w-[659px]">
+        Наши авторы — лауреаты региональных творческих конкурсов, обладатели
+        премий федерального масштаба, за плечами у них публикации в солидных
+        журналах и альманахах, изданные книги, членство в профессиональных
+        союзах, мы постоянно в тренде, и продолжаем оттачивать свое мастерство.
+      </p>
+      <div className="mt-3">
+        <Carousel items={authors} />
+      </div>
+    </section>
+  );
 }
--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -1,9 +1,9 @@
-"use client"
+"use client";
 import { useState } from "react";
 import HeaderContent from "./header-content";
 import Menu from "./menu";
-import { Sheet, SheetTrigger, SheetContent } from "@/components/ui/sheet"; 
-import { MenuIcon } from "lucide-react"; 
+import { Sheet, SheetTrigger, SheetContent } from "@/components/ui/sheet";
+import { MenuIcon } from "lucide-react";
 import Image from "next/image";
 import { Button } from "../ui/button";
 
@@ -17,21 +17,11 @@ export default function Header() {
           <button onClick={() => setIsOpen(true)}>
             <MenuIcon className="w-6 h-6 text-orange-600" />
           </button>
-          
-          <Image 
-            src="/logo.svg"
-            alt="Логотип"
-            width={138}
-            height={44}
-          />
+
+          <Image src="/logo.svg" alt="Логотип" width={138} height={44} />
 
           <button>
-            <Image
-              src="/WhatsApp.svg"
-              alt="WhatsApp"
-              width={25}
-              height={25}
-            />
+            <Image src="/WhatsApp.svg" alt="WhatsApp" width={25} height={25} />
           </button>
         </div>
 
@@ -44,16 +34,27 @@ export default function Header() {
               <h4 className="text-[16px] regular">Пообщаемся?</h4>
               <div className="flex flex-row items-center mt-2">
                 <Image src="/phone.svg" alt="Телефон" width={26} height={25} />
-                <span className="text-[20px] text-[#25253F] font-semibold ml-2">+7 999 333-89-60</span>
+                <span className="text-[20px] text-[#25253F] font-semibold ml-2">
+                  +7 999 333-89-60
+                </span>
               </div>
               <hr />
-              <Button variant="callback" className="mt-4 w-full">ОБРАТНЫЙ ЗВОНОК</Button>
+              <Button variant="callback" className="mt-4 w-full">
+                ОБРАТНЫЙ ЗВОНОК
+              </Button>
               <div className="flex items-center gap-3 mt-4">
                 <Image src="/vk.svg" alt="VK" width={25} height={25} />
                 <Image src="/tg.svg" alt="Telegram" width={25} height={25} />
-                <Image src="/WhatsApp.svg" alt="WhatsApp" width={25} height={25} />
+                <Image
+                  src="/WhatsApp.svg"
+                  alt="WhatsApp"
+                  width={25}
+                  height={25}
+                />
               </div>
-              <p className="text-center text-orange-600 mt-4">mail@rifmach.ru</p>
+              <p className="text-center text-orange-600 mt-4">
+                mail@rifmach.ru
+              </p>
             </div>
           </SheetContent>
         </Sheet>
