8f2d0c57728f6a75cbf909dfd1d8c16bb0d6cf44
terrxrmachine <so3.workspace@gmail.com>
2024-10-23 19:40:54 +0400

    хотфикс родмапа

--- components/main-section/roadmap-secion.tsx
+++ components/main-section/roadmap-secion.tsx
@@ -1,22 +1,26 @@
-import StepsCards from "../shared/steps-cards";
-import { Button } from "../ui/button";
+import StepsCards from "../shared/steps-cards"
+import { Button } from "../ui/button"
 
 export default function RoadmapSection() {
   return (
-    <section className="bg-white mx-[17px] md:mx-[134px] pt-[90px] pb-[45px] md:py-[25px]">
-      <h2 className="text-headers text-[24px] md:text-h2 uppercase">
-        Как создается стих,
-        <br />
-        бьющий точно в цель
+    <section className="bg-white mx-4 md:px-10 lg:px-20 xl:px-36 2xl:px-60 py-20 md:py-6">
+      <h2 className="text-headers text-2xl md:text-h2 uppercase">
+        Как создается стих,<br />
+        <span className="font-bold">бьющий точно в цель?</span>
       </h2>
-      <hr className="w-[40px] md:w-[50px] h-[5px] bg-main rounded-sm mt-[10px] md:mt-[22px]" />
+      
+      <hr className="w-10 md:w-[50px] h-[5px] bg-main rounded-sm mt-2.5 md:mt-5"/>
+      
       <StepsCards />
-      <center>
-        <Button variant="secondary" className="mt-0 md:mt-[40px]">
-          {" "}
+      
+      <div className="text-center">
+        <Button 
+          variant="secondary" 
+          className="md:mt-10 uppercase"
+        >
           НА КАКИЕ ТЕМЫ МЫ ПИШЕМ?
         </Button>
-      </center>
+      </div>
     </section>
-  );
+  )
 }
