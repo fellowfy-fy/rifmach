1a8dc4a330ca464cf4e8e2a2e842c627b5fde50e
fellowfy-fy <zhoraawesome@gmail.com>
2024-11-15 11:39:01 +0700

    Рифмача отменили

--- components/ui/CustomButton/CustomButton.types.ts
+++
@@ -1,8 +0,0 @@
-import { ReactNode, CSSProperties } from 'react';
-
-export interface ICustomButtonProps {
-  type?: 'main' | 'secondary';
-  children: ReactNode;
-  customClass?: string;
-  onClick?: ()=> void;
-}
